a4e329ed7a75b91c4ebbdf5f6ceb82661b3050c7
M0M3NTUM <91469978+M0M3NTUM44@users.noreply.github.com>
2023-04-04 18:11:21 +0300

    Update TestScript.ps1

--- TestScript.ps1
+++ TestScript.ps1
@@ -1,5 +1,4 @@
 #Benign test script to test if the Bypass was seccussful.
 Write-Host "amsiutils"
 Write-Host "[+] If you see this AMSI is Bypassed on the current PS shell"
-Write-Host "[+] You should change the 'AMSIReflectionCrypt.ps1' script in line 55 to run your own powershell scripts"
-
+Write-Host "[+] Use the -u argument to run your own powershell scripts in memory"
