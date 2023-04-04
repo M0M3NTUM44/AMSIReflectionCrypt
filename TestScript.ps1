782ca063a44930676da66455bfaf541245c950fd
M0M3NTUM44 <lahavisme@gmail.com>
2023-04-04 13:08:22 +0300

    4.4.23

--- TestScript.ps1
+++ TestScript.ps1
@@ -1,3 +1,4 @@
 # Benign test script to test if the Bypass was seccussful(This will tyoe your username to terminal).
 $username = $env:USERNAME
-Write-Host "Current user: $username"
+$evilString = "Invoke Mimikatz"
+Write-Host "$username $evilString if you see this AMSI is Bypassed"
