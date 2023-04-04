04c8588c23f94cd98d37b06141f8cf4afd1246bd
M0M3NTUM44 <lahavisme@gmail.com>
2023-04-04 15:42:38 +0300

    4.4.23

--- TestScript.ps1
+++ TestScript.ps1
@@ -1,4 +1,5 @@
-# Benign test script to test if the Bypass was seccussful(This will tyoe your username to terminal).
-$username = $env:USERNAME
-Write-Host "$username if you see this AMSI is Bypassed"
+#Benign test script to test if the Bypass was seccussful.
+Write-Host "amsiutils"
+Write-Host "If you see this AMSI is Bypassed on the current PS proccess"
 Write-Host "You should change the 'AMSIReflectionCrypt.ps1' script in line 55 to run your own powershell commands"
+
