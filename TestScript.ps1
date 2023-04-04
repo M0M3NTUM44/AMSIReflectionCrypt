f2267ddc9183e2f851a1bb277cd44a4a05324441
M0M3NTUM44 <lahavisme@gmail.com>
2023-04-04 12:56:16 +0300

    4.4.23

--- TestScript.ps1
+++ TestScript.ps1
@@ -1,3 +1,3 @@
-# Benign test script to test if the Bypass was seccussful(This will connect to an Api to print your Public IP address).
-set-content "X5O!P%@AP[4`\PZX54(P^)7CC)7}`$EICAR-STANDARD-ANTIVIRUS-TEST-FILE!`$H+H*" -path "eicar.com"
-
+# Benign test script to test if the Bypass was seccussful(This will tyoe your username to terminal).
+$username = $env:USERNAME
+Write-Host "Current user: $username"
